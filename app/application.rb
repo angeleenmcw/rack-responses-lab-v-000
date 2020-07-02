9999c376cf21649ac720849334d6b61521a79671
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-07-02 04:01:09 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,9 +3,9 @@ class Application
   def call(env)
     resp = Rack::Reponse.new 
     
-    time_1 = Time.now.hour
+    time = Time.now.hour
     
-    if time_1 > "12:00"
+    if time > "12:00"
       resp.write("Good Morning!")
     else
       resp.write("Good Afternoon!")
