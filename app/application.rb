bfc962ff4469711206a80fb0b43645e39ff63810
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-07-02 03:58:36 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,9 +6,9 @@ class Application
     time_1 = Time.now.strftime("%H:%M")
     
     if time_1 > "12:00"
-      resp.write "Good Morning!"
+      resp.write("Good Morning!")
     else
-      resp.write "Good Afternoon!"
+      resp.write("Good Afternoon!")
     end
     
     resp.finish
