1b5fe4d5c40f4c9acb43aa9ae9fee5dd728cb2d0
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-07-02 04:02:16 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,9 +7,7 @@ class Application
     
     if time < 12 ? greeting = "Good Morning!" : greeting = "Good Afternoon!"
       resp.write("Good Morning!")
-    else
-      resp.write("Good Afternoon!")
-    end
+
     
     resp.finish
     
